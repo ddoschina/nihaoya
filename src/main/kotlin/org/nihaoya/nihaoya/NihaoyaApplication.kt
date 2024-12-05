4f016d197e57a8864fc0b774075c9a1354537fbd
李志涌 <1484333745@qq.com>
2024-12-05 09:13:20 +0800

    init

---
+++ src/main/kotlin/org/nihaoya/nihaoya/NihaoyaApplication.kt
@@ -0,0 +1,11 @@
+package org.nihaoya.nihaoya
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NihaoyaApplication
+
+fun main(args: Array<String>) {
+    runApplication<NihaoyaApplication>(*args)
+}
---
+++ src/test/kotlin/org/nihaoya/nihaoya/NihaoyaApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.nihaoya.nihaoya
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NihaoyaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
